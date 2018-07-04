6f9034be6021e943a2b27bbb1f43fce2fb6be040
wickinot <27719504+wickinot@users.noreply.github.com>
2018-07-04 15:36:17 +1000

    begin-rescue test trial

--- ex48/lib/ex48/lexicon.rb
+++ ex48/lib/ex48/lexicon.rb
@@ -1,18 +1,30 @@
 class Lexicon
 #scan user input and make it into a variable
-	def scan(user_input)		
-  		@user_input = user_input
+	def scan(answer)		
+		# take raw input from user
+  		begin
+  			#if word is part of lexicon return sentence composed of array of arrays with (token,word pairing)
+  			#break up the answer into words
+  			answer = $stdin.gets.chomp
+  			words = answer.split
+  			return direction(word)
+  		rescue
+  			#if word not part of lexicon - return error token
+  			return nil
+  		end
   	end
 
+
+
   	def direction
-  		@direction = direction
+  		@direction = [north, south, east, west, down, up, left, right, back]
   	end
 
   #take user_input and sort it into a [type,word] pair
   	
- 	def path(user_input)
- 		return [direction, user_input]
- 	end
+ 	# def path(user_input)
+ 	# 	return [direction, user_input]
+ 	# end
 
 end
 
