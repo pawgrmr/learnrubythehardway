9f8d83edebaf981124ff9c0198ae472260139e62
wickinot <27719504+wickinot@users.noreply.github.com>
2018-07-11 16:12:54 +1000

    test paths working so far

--- ex48/lib/ex48/lexicon.rb
+++ ex48/lib/ex48/lexicon.rb
@@ -1,30 +1,35 @@
 class Lexicon
-#scan user input and make it into a variable
-	def scan(answer)		
-		# take raw input from user
-  		begin
-  			#if word is part of lexicon return sentence composed of array of arrays with (token,word pairing)
-  			#break up the answer into words
-  			answer = $stdin.gets.chomp
-  			words = answer.split
-  			return direction(word)
-  		rescue
-  			#if word not part of lexicon - return error token
-  			return nil
-  		end
-  	end
+# #scan user input and make it into a variable
+# 	@@type == {
+# 		'direction' => [['direction','north'], 'south', 'east', 'west', 'down', 'up', 'left', 'right', 'back']
+# 	}
+def self.scan
+end
+# 	def self.scan(word)		
+# 		# take raw input from user
+# 		if word == 'direction'
+# 			return true
+# 		else
+# 			return nil
+  		
+#   			#if word is part of lexicon return sentence composed of array of arrays with (token,word pairing)
+#   			#break up the answer into word
+#   			# should return direction if word == direction lexicon
+  			
+#   			#if word not part of lexicon - return error token
+  			
+#   		end
+#   	end
 
 
 
-  	def direction
-  		@direction = [north, south, east, west, down, up, left, right, back]
-  	end
+  
 
-  #take user_input and sort it into a [type,word] pair
+#   #take user_input and sort it into a [type,word] pair
   	
- 	# def path(user_input)
- 	# 	return [direction, user_input]
- 	# end
+#  	# def path(user_input)
+#  	# 	return [direction, user_input]
+#  	# end
 
 end
 
--- ex48/tests/test_lexicon.rb
+++ ex48/tests/test_lexicon.rb
@@ -4,7 +4,12 @@ require "test/unit"
 
 class TestLexicon < Test::Unit::TestCase
 
-	def test_directions()
-		assert_equal(Lexicon.scan("north"), [['direction', 'north']])
-	end 
+  def test_directions()
+    assert_equal(Lexicon.scan("north"), [['direction', 'north']])
+    # result = Lexicon.scan("north south east")
+
+    # assert_equal(result, [['direction', 'north'],
+    #        ['direction', 'south'],
+    #        ['direction', 'east']])
+  end
 end
