e4e7e5beda57f16594e5fd3002bec9efcceae551
wickinot <27719504+wickinot@users.noreply.github.com>
2018-07-13 13:53:07 +1000

    trial code set 2

--- ex48/lib/ex48/lexicon.rb
+++ ex48/lib/ex48/lexicon.rb
@@ -1,36 +1,45 @@
 class Lexicon
-# #scan user input and make it into a variable
-# 	@@type == {
-# 		'direction' => [['direction','north'], 'south', 'east', 'west', 'down', 'up', 'left', 'right', 'back']
-# 	}
-def self.scan
+
+	# def self.scan(word)
+	# 	#scan for the word and place it into an array of array [token, word] pairing
+	# 	#?trial a while loop => while word == @@direction key return []
+
+	# 	@@direction = { #assign hash to class var @@direction
+	# 		"north" => ['direction', 'north'],
+	# 		"south" => ['direction', 'south'],
+	# 		"east" => ['direction', 'east']
+	# 	}
+
+	# 	pair = @@direction.assoc("word") #do the same for the words north, south and east - convert user input words for direction into [token word] pair.
+	# 	elements = [] #want to push the array of token, word pairing to this array
+
+	# 	elements.each do |element|
+	# 		if word == h.key(word) #word == hash key 			
+	# 			elements.push(h.value)
+	# 		else 
+	# 			return nil
+	# 		end
+	# 	end
+	# end
+
+def self.scan(sentence)
+
+result = sentence.split
+
+	hash = {
+		'direction' => ['north', 'south', 'east']
+	}
+	
+	elements = [['direction', 'north']]
+
+	hash.each do |key, value|
+		if result == "#{value}"
+			element.push("[#{key}, #{value}]")
+		else
+			return nil
+		end
+	end
 end
-# 	def self.scan(word)		
-# 		# take raw input from user
-# 		if word == 'direction'
-# 			return true
-# 		else
-# 			return nil
-  		
-#   			#if word is part of lexicon return sentence composed of array of arrays with (token,word pairing)
-#   			#break up the answer into word
-#   			# should return direction if word == direction lexicon
-  			
-#   			#if word not part of lexicon - return error token
-  			
-#   		end
-#   	end
-
-
-
-  
-
-#   #take user_input and sort it into a [type,word] pair
-  	
-#  	# def path(user_input)
-#  	# 	return [direction, user_input]
-#  	# end
-
 end
 
 
@@ -38,9 +47,23 @@ end
 
 
 
+#take each of the words and #if word matches the value in the direction hash, 
+#return it as an array of array with (word, [[direction, word]]) pairing.
+#push this into the empty array
 
 
+# require 'ex48/lexicon.rb'
+# require "test/unit"
 
 
+# class TestLexicon < Test::Unit::TestCase
 
+#   def test_directions()
+#     assert_equal(Lexicon.scan("north"), [['direction', 'north']])
+#     result = Lexicon.scan("north south east") #split these words
 
+#     assert_equal(result, [['direction', 'north'], #i.e. assert equal(result, [])
+#            ['direction', 'south'],
+#            ['direction', 'east']])
+#   end
+# end
