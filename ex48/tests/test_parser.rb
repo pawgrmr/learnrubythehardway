6ac21d161f2d05c5e90657a9cff194c1c5fad656
wickinot <27719504+wickinot@users.noreply.github.com>
2018-07-30 16:31:55 +1000

    initiated testing for ex49

---
+++ ex48/tests/test_parser.rb
@@ -0,0 +1,10 @@
+# What You Should Test
+
+# For Exercise 49, write a complete test that confirms everything in this code is working. That includes making exceptions happen by giving the parser bad sentences.
+
+# Check for an exception by using the function assert_raise from the Test::Unit documentation. Learn how to use this so you can write a test that is expected to fail, which is very important in testing. Learn about this function (and others) by reading the Test::Unit documentation.
+
+# When you are done, you should know how this bit of code works and how to write a test for other people's code even if they do not want you to. Trust me, it's a very handy skill to have.
+
+require "ex48/parser.rb"
+require "test/unit"
