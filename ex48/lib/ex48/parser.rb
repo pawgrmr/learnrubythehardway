0ffddfa2e22e34cbf4edc86b182ee0b9e9bd5a34
wickinot <27719504+wickinot@users.noreply.github.com>
2018-07-31 14:17:40 +1000

    Testing for parser

--- ex48/lib/ex48/parser.rb
+++ ex48/lib/ex48/parser.rb
@@ -1,109 +1,115 @@
+
 class ParserError < Exception 
 end
 #convert scanned sentence into array of sentence objects 
-	#loop through scanned words
+  #loop through scanned words
 
-	#match types of tuples 
+  #match types of tuples 
 
-	#peek at a potential tuple: look at the next element in our tuple array, and then match to take one off and work with it.
+  #peek at a potential tuple: look at the next element in our tuple array, and then match to take one off and work with it.
 
-	#skip things dont care about
+  #skip things dont care about
 
-	#sentence object to put results in. Needs to have 3 attributes:
-		#Sentence.subject - noun
-		#Sentence.verb - verb
-		#Sentence.object - noun
+  #sentence object to put results in. Needs to have 3 attributes:
+    #Sentence.subject - noun
+    #Sentence.verb - verb
+    #Sentence.object - noun
 
 
 class Sentence
 
-	def initialize(subject, verb, object)
-		# take the array pairs ['noun', 'princess'] and convert them
-		@subject = subject[1] #i.e. this will be princess and so on
-		@verb = verb[1]
-		@object = obj[1]
-	end
+  def initialize(subject, verb, object)
+    # take the array pairs ['noun', 'princess'] and convert them
+    @subject = subject[1] #i.e. this will be princess and so on
+    @verb = verb[1]
+    @object = object[1]
+  end
 
-	attr_reader :subject
-	attr_reader :verb
-	attr_reader :object	
-
-	def peek(word_list)
-		#peek at a list of words and return the type of word it is
-		if word_list
-			word = word_list[0]
-			return word[0]
-		else
-			return nil
-		end
-	end
-
-	def match(word_list, expecting)
-		if word_list
-			word = word_list.shift #take the word off the list
-
-			if word[0] == expecting #confirm word is the right type
-				return word 
-			else
-				return nil
-			end
-		else
-			return nil
-		end
-	end
-
-	def skip(word_list, word_type)
-		#skip words that arent going to be useful in the Sentence - "stop words"
-		while peek(word_list) == word_type
-			match(word_list, word_type)
-		end
-	end
-
-	def parse_verb(word_list)
-		skip(word_list, 'stop') # skip all the stop words (see lexicon.rb)
-
-		if peek(word_list) == 'verb'
-			return match(word_list, 'verb')
-		else
-			raise ParserError.new("Expected a verb next.")
-		end
-	end
-
-	def parse_object(word_list)
-		skip(word_list, 'stop')
-		next_word = peek(word_list)
-
-		if next_word == 'noun'
-			return match(word_list, 'noun')
-		elsif next_word == 'direction'
-			return match(word_list, 'direction')
-		else
-			raise ParserError.new("Expected a noun or direction next.")
-		end
-	end
-
-	def parse_subject(word_list)
-	skip(word_list, 'stop')
-	next_word = peek(word_list)
-
-		if next_word == 'noun'
-			return match(word_list, 'noun')
-		elsif next_word == 'verb'
-			return ['noun', 'player']
-		else
-			raise ParserError.new("expected a verb next.")
-		end
-	end
-
-	def parse_sentence(word_list)
-		subj = parse_subject(word_list)
-		verb = parse_verb(word_list)
-		obj = parse_object(word_list)
-	
-
-		return Sentence.new(subj, verb, obj)
-	end
+  attr_reader :subject
+  attr_reader :verb
+  attr_reader :object 
 
 
+
+  def peek(word_list)
+    #peek at a list of words and return the type of word it is
+      if word_list
+        word = word_list[0]
+        return word[0]
+      else
+        return nil
+      end
+  end
+
+
+  def match(word_list, expecting)
+      if word_list
+        word = word_list.shift #take the word off the list
+
+          if word[0] == expecting #confirm word is the right type
+            return word 
+          else
+            return nil
+          end
+      else
+        return nil
+      end
+  end
+
+
+  def skip(word_list, word_type)
+    #skip words that arent going to be useful in the Sentence - "stop words"
+    while peek(word_list) == word_type
+      match(word_list, word_type)
+    end
+  end
+
+
+  def parse_subject(word_list)
+  skip(word_list, 'stop')
+  next_word = peek(word_list)
+
+      if next_word == 'noun'
+        return match(word_list, 'noun')
+      elsif next_word == 'verb'
+        return ['noun', 'player']
+      else
+        raise ParserError.new("Expected a verb next.")
+      end
+  end
+
+  def parse_verb(word_list)
+    skip(word_list, 'stop') # skip all the stop words (see lexicon.rb)
+
+      if peek(word_list) == 'verb'
+        return match(word_list, 'verb')
+      else
+        raise ParserError.new("Expected a verb next.")
+      end
+  end
+
+
+  def parse_object(word_list)
+    skip(word_list, 'stop')
+    next_word = peek(word_list)
+
+      if next_word == 'noun'
+        return match(word_list, 'noun')
+      elsif next_word == 'direction'
+        return match(word_list, 'direction')
+      else
+        raise ParserError.new("Expected a noun or direction next.")
+      end
+  end
+
+
+  def parse_sentence(word_list)
+    subject = parse_subject(word_list)
+    verb = parse_verb(word_list)
+    object = parse_object(word_list)
+  
+
+    return Sentence.new(subject, verb, object)
+  end
 
 end
--- ex48/tests/test_parser.rb
+++ ex48/tests/test_parser.rb
@@ -1,10 +1,42 @@
-# What You Should Test
+# For Exercise 49, write a complete test that confirms everything in this code is working. 
+# Put the test in tests/test_parser.rb similar to the test file from the last exercise. 
+# That includes making exceptions happen by giving the parser bad sentences.
 
-# For Exercise 49, write a complete test that confirms everything in this code is working. That includes making exceptions happen by giving the parser bad sentences.
+# Check for an exception by using the function assert_raise from the Test::Unit documentation. 
+# Learn how to use this so you can write a test that is expected to fail, which is very important in testing. 
+# Learn about this function (and others) by reading the Test::Unit documentation.
 
-# Check for an exception by using the function assert_raise from the Test::Unit documentation. Learn how to use this so you can write a test that is expected to fail, which is very important in testing. Learn about this function (and others) by reading the Test::Unit documentation.
-
-# When you are done, you should know how this bit of code works and how to write a test for other people's code even if they do not want you to. Trust me, it's a very handy skill to have.
-
-require "ex48/parser.rb"
+require 'ex48/parser.rb'
 require "test/unit"
+
+
+class TestParser < Test::Unit::TestCase
+
+def test_peek()
+	assert_equal(Sentence.peek(["verb", "run"]), "verb")
+	result = Sentence.peek(["verb", "run"])
+	assert_equal(result, ["verb"])
+end
+
+def test_match()
+	assert_equal(Sentence.match([["noun", "princess"], ["verb, eat"]]), ["noun", "princess"], "noun")
+	result = Sentence.match([["noun", "princess"], ["verb, eat"]])
+	assert_equal(result, ["noun"])
+end
+
+def test_skip
+end
+
+def test_subject
+end
+
+def test_verb
+end
+
+def test_object
+end
+
+def test_sentence
+end
+
+end
